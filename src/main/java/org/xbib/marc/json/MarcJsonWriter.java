0c213a2dd44c5166b8b598615f30a4ed06f9cccc
Thomas Gängler <thomas.gaengler@slub-dresden.de>
2018-08-17 10:39:50 +0200

    added \n etc. explicitly to content escaping in MarcJsonWriter

--- src/main/java/org/xbib/marc/json/MarcJsonWriter.java
+++ src/main/java/org/xbib/marc/json/MarcJsonWriter.java
@@ -161,6 +161,26 @@ public class MarcJsonWriter extends MarcContentHandler implements Flushable, Clo
                     sb.append("\\\\");
 
                     break;
+                case '\b':
+                    sb.append("\\b");
+
+                    break;
+                case '\f':
+                    sb.append("\\f");
+
+                    break;
+                case '\n':
+                    sb.append("\\n");
+
+                    break;
+                case '\r':
+                    sb.append("\\r");
+
+                    break;
+                case '\t':
+                    sb.append("\\t");
+
+                    break;
                 default:
                     if (c < 0x1f) {
 
