1e61733ae56f81a0a0151033bda2821383928670
Jörg Prante <joergprante@gmail.com>
2022-10-16 09:36:44 +0200

    update copyright header

--- src/main/java/org/xbib/marc/xml/IndentingXMLEventWriter.java
+++ src/main/java/org/xbib/marc/xml/IndentingXMLEventWriter.java
@@ -1,18 +1,17 @@
-/*
-   Copyright 2016 Jörg Prante
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
+/**
+ *  Copyright 2016-2022 Jörg Prante <joergprante@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</a>
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.xbib.marc.xml;
 
@@ -34,8 +33,6 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.XMLEvent;
 
-/**
- */
 public class IndentingXMLEventWriter implements XMLEventWriter {
 
     private static final Logger logger = Logger.getLogger(IndentingXMLEventWriter.class.getName());
--- src/test/java/org/xbib/marc/dialects/mab/MabXmlTest.java
+++ src/test/java/org/xbib/marc/dialects/mab/MabXmlTest.java
@@ -1,6 +1,20 @@
+/**
+ *  Copyright 2016-2022 Jörg Prante <joergprante@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</a>
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.xbib.marc.dialects.mab;
 
-import static org.xbib.marc.StreamMatcher.assertStream;
 import org.junit.jupiter.api.Test;
 import org.xbib.marc.Marc;
 import org.xbib.marc.StreamMatcher;
@@ -9,9 +23,6 @@ import org.xbib.marc.json.MarcJsonWriter;
 import org.xbib.marc.xml.MarcContentHandler;
 import org.xbib.marc.xml.MarcXchangeWriter;
 import org.xml.sax.InputSource;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.EnumSet;
 
