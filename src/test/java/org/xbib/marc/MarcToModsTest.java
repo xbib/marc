392a61dc1251fb2b7e166d60275a593f9d7f9819
Jörg Prante <joergprante@gmail.com>
2022-11-07 22:29:56 +0100

    in MODS test, replace external XSL loading from loc.gov by internal XSL

--- src/test/java/org/xbib/marc/MarcToModsTest.java
+++ src/test/java/org/xbib/marc/MarcToModsTest.java
@@ -45,163 +45,175 @@ public class MarcToModsTest {
     @Test
     public void testSax() throws Exception {
         String s = "summerland.mrc";
-        InputStream marcInputStream = getClass().getResource(s).openStream();
-        Marc marc = Marc.builder()
-                .setInputStream(marcInputStream)
-                .setCharset(Charset.forName("ANSEL"))
-                .setSchema(MARC21_FORMAT)
-                .build();
-        Source source = new SAXSource(marc.iso2709XmlReader(), new InputSource(marcInputStream));
-        StringWriter writer = new StringWriter();
-        StreamResult streamResult = new StreamResult(writer);
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        transformer.transform(source, streamResult);
-        assertThat(writer.toString(),
-                CompareMatcher.isIdenticalTo(getClass().getResource("summerland-sax-marc.xml").openStream()));
+        try (InputStream marcInputStream = getClass().getResource(s).openStream()) {
+            Marc marc = Marc.builder()
+                    .setInputStream(marcInputStream)
+                    .setCharset(Charset.forName("ANSEL"))
+                    .setSchema(MARC21_FORMAT)
+                    .build();
+            Source source = new SAXSource(marc.iso2709XmlReader(), new InputSource(marcInputStream));
+            StringWriter writer = new StringWriter();
+            StreamResult streamResult = new StreamResult(writer);
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.transform(source, streamResult);
+            assertThat(writer.toString(),
+                    CompareMatcher.isIdenticalTo(getClass().getResource("summerland-sax-marc.xml").openStream()));
+        }
     }
 
     @Test
     public void testDom() throws Exception {
         String s = "summerland.mrc";
-        InputStream marcInputStream = getClass().getResource(s).openStream();
-        Marc marc = Marc.builder()
-                .setInputStream(marcInputStream)
-                .setCharset(Charset.forName("ANSEL"))
-                .setSchema(MARC21_FORMAT)
-                .build();
-        Sax2Dom sax2Dom = new Sax2Dom(marc.iso2709XmlReader(), new InputSource(marcInputStream));
-        Document document = sax2Dom.document();
-        StringWriter writer = new StringWriter();
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        transformer.transform(new DOMSource(document), new StreamResult(writer));
-        assertThat(writer.toString(),
-                CompareMatcher.isIdenticalTo(getClass().getResource("summerland-dom-marc.xml").openStream()));
+        try (InputStream marcInputStream = getClass().getResource(s).openStream()) {
+            Marc marc = Marc.builder()
+                    .setInputStream(marcInputStream)
+                    .setCharset(Charset.forName("ANSEL"))
+                    .setSchema(MARC21_FORMAT)
+                    .build();
+            Sax2Dom sax2Dom = new Sax2Dom(marc.iso2709XmlReader(), new InputSource(marcInputStream));
+            Document document = sax2Dom.document();
+            StringWriter writer = new StringWriter();
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.transform(new DOMSource(document), new StreamResult(writer));
+            assertThat(writer.toString(),
+                    CompareMatcher.isIdenticalTo(getClass().getResource("summerland-dom-marc.xml").openStream()));
+        }
     }
 
     @Test
     public void testStylesheetSax() throws Exception {
         String s = "summerland.mrc";
-        InputStream marcInputStream = getClass().getResource(s).openStream();
-        Marc marc = Marc.builder()
-                .setInputStream(marcInputStream)
-                .setCharset(Charset.forName("ANSEL"))
-                .setSchema(MARC21_FORMAT)
-                .build();
-        Source source = new SAXSource(marc.iso2709XmlReader(), new InputSource(marcInputStream));
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        StreamResult streamResult = new StreamResult(out);
-        InputStream xslInputStream = getClass().getResourceAsStream("MARC21slim2MODS3-6.xsl");
-        TransformerFactory factory = TransformerFactory.newInstance();
-        factory.setURIResolver(new ClasspathResourceURIResolver());
-        Transformer transformer = factory.newTemplates(new StreamSource(xslInputStream)).newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        transformer.setErrorListener(new ErrorListener() {
-            @Override
-            public void warning(TransformerException exception) throws TransformerException {
-                exception.printStackTrace();
-            }
+        try (InputStream marcInputStream = getClass().getResource(s).openStream()) {
+            Marc marc = Marc.builder()
+                    .setInputStream(marcInputStream)
+                    .setCharset(Charset.forName("ANSEL"))
+                    .setSchema(MARC21_FORMAT)
+                    .build();
+            Source source = new SAXSource(marc.iso2709XmlReader(), new InputSource(marcInputStream));
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            StreamResult streamResult = new StreamResult(out);
+            InputStream xslInputStream = getClass().getResourceAsStream("MARC21slim2MODS3-6.xsl");
+            TransformerFactory factory = TransformerFactory.newInstance();
+            // required for realtive URI resolving in xsl:include
+            factory.setURIResolver(new ClasspathResourceURIResolver());
+            Transformer transformer = factory.newTemplates(new StreamSource(xslInputStream)).newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.setErrorListener(new ErrorListener() {
+                @Override
+                public void warning(TransformerException exception) throws TransformerException {
+                    exception.printStackTrace();
+                }
 
-            @Override
-            public void error(TransformerException exception) throws TransformerException {
-                exception.printStackTrace();
-            }
+                @Override
+                public void error(TransformerException exception) throws TransformerException {
+                    exception.printStackTrace();
+                }
 
-            @Override
-            public void fatalError(TransformerException exception) throws TransformerException {
-                exception.printStackTrace();
-            }
-        });
-        transformer.transform(source, streamResult);
-        marcInputStream.close();
-        xslInputStream.close();
-        assertThat(out.toByteArray(),
-                CompareMatcher.isIdenticalTo(getClass().getResource("summerland-sax-mods.xml").openStream()));
+                @Override
+                public void fatalError(TransformerException exception) throws TransformerException {
+                    exception.printStackTrace();
+                }
+            });
+            transformer.transform(source, streamResult);
+            marcInputStream.close();
+            xslInputStream.close();
+            assertThat(out.toByteArray(),
+                    CompareMatcher.isIdenticalTo(getClass().getResource("summerland-sax-mods.xml").openStream()));
+        }
     }
 
     @Test
     public void testStylesheetDom() throws Exception {
         String s = "summerland.mrc";
-        InputStream marcInputStream = getClass().getResource(s).openStream();
-        Marc marc = Marc.builder()
-                .setInputStream(marcInputStream)
-                .setCharset(Charset.forName("ANSEL"))
-                .setSchema(MARC21_FORMAT)
-                .build();
-        Sax2Dom sax2Dom = new Sax2Dom(marc.iso2709XmlReader(), new InputSource(marcInputStream));
-        Document document = sax2Dom.document();
-        Source source = new DOMSource(document);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        StreamResult streamResult = new StreamResult(out);
-        InputStream xslInputStream = getClass().getResourceAsStream("MARC21slim2MODS3-6.xsl");
-        TransformerFactory factory = TransformerFactory.newInstance();
-        factory.setURIResolver(new ClasspathResourceURIResolver());
-        Transformer transformer = factory.newTemplates(new StreamSource(xslInputStream)).newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        transformer.setErrorListener(new ErrorListener() {
-            @Override
-            public void warning(TransformerException exception) throws TransformerException {
-                exception.printStackTrace();
-            }
+        try (InputStream marcInputStream = getClass().getResource(s).openStream()) {
+            Marc marc = Marc.builder()
+                    .setInputStream(marcInputStream)
+                    .setCharset(Charset.forName("ANSEL"))
+                    .setSchema(MARC21_FORMAT)
+                    .build();
+            Sax2Dom sax2Dom = new Sax2Dom(marc.iso2709XmlReader(), new InputSource(marcInputStream));
+            Document document = sax2Dom.document();
+            Source source = new DOMSource(document);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            StreamResult streamResult = new StreamResult(out);
+            InputStream xslInputStream = getClass().getResourceAsStream("MARC21slim2MODS3-6.xsl");
+            TransformerFactory factory = TransformerFactory.newInstance();
+            factory.setURIResolver(new ClasspathResourceURIResolver());
+            Transformer transformer = factory.newTemplates(new StreamSource(xslInputStream)).newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.setErrorListener(new ErrorListener() {
+                @Override
+                public void warning(TransformerException exception) throws TransformerException {
+                    exception.printStackTrace();
+                }
 
-            @Override
-            public void error(TransformerException exception) throws TransformerException {
-                exception.printStackTrace();
-            }
+                @Override
+                public void error(TransformerException exception) throws TransformerException {
+                    exception.printStackTrace();
+                }
 
-            @Override
-            public void fatalError(TransformerException exception) throws TransformerException {
-                exception.printStackTrace();
-            }
-        });
-        transformer.transform(source, streamResult);
-        marcInputStream.close();
-        xslInputStream.close();
-        assertThat(out.toByteArray(),
-                CompareMatcher.isIdenticalTo(getClass().getResource("summerland-dom-mods.xml").openStream()));
+                @Override
+                public void fatalError(TransformerException exception) throws TransformerException {
+                    exception.printStackTrace();
+                }
+            });
+            transformer.transform(source, streamResult);
+            marcInputStream.close();
+            xslInputStream.close();
+            assertThat(out.toByteArray(),
+                    CompareMatcher.isIdenticalTo(getClass().getResource("summerland-dom-mods.xml").openStream()));
+        }
     }
 
+    /**
+     * With regard to sandboxed CI/CD platforms, we avoid loading external XSL style sheets from loc.gov
+     * We run the test with a local copy.
+     *
+     * @throws Exception if test fails
+     */
     @Test
-    public void testLocStyleSheet() throws Exception {
+    public void testLocLocalCopyStyleSheet() throws Exception {
         String s = "summerland.mrc";
-        InputStream marcInputStream = getClass().getResource(s).openStream();
-        Marc marc = Marc.builder()
-                .setInputStream(marcInputStream)
-                .setCharset(Charset.forName("ANSEL"))
-                .setSchema(MARC21_FORMAT)
-                .build();
-        StringWriter sw = new StringWriter();
-        Result result = new StreamResult(sw);
-        System.setProperty("http.agent", "Java Agent");
-        marc.transform(TransformerFactory.newInstance(),
-                new URL("http://www.loc.gov/standards/mods/v3/MARC21slim2MODS3.xsl"), result);
-        assertThat(sw.toString(),
-                CompareMatcher.isIdenticalTo(getClass().getResource("summerland-mods-loc-goc.xml").openStream()));
+        try (InputStream marcInputStream = getClass().getResource(s).openStream()) {
+            Marc marc = Marc.builder()
+                    .setInputStream(marcInputStream)
+                    .setCharset(Charset.forName("ANSEL"))
+                    .setSchema(MARC21_FORMAT)
+                    .build();
+            StringWriter sw = new StringWriter();
+            Result result = new StreamResult(sw);
+            URL url = getClass().getResource("MARC21slim2MODS3.xsl");
+            TransformerFactory factory = TransformerFactory.newInstance();
+            factory.setURIResolver(new ClasspathResourceURIResolver());
+            marc.transform(factory, url, result);
+            assertThat(sw.toString(),
+                    CompareMatcher.isIdenticalTo(getClass().getResource("summerland-mods-loc-goc-local-copy.xml").openStream()));
+        }
     }
-
 
     private static class ClasspathResourceURIResolver implements URIResolver {
         @Override
-        public Source resolve(String href, String base) throws TransformerException {
+        public Source resolve(String href, String base) {
             return new StreamSource(getClass().getResourceAsStream(href));
         }
     }
